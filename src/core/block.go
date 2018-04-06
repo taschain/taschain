00b72a4ec5bb6239c4ad53270d102ea7b532da58
tequila <114778836@qq.com>
2018-04-06 17:42:26 +0800

    blockchain 基本实现

--- src/core/block.go
+++ src/core/block.go
@@ -10,12 +10,15 @@ import (
 type BlockHeader struct {
 	//Trans_Hash_List：交易集哈希列表
 	//Trans_Root_Hash：默克尔树根哈希
+	Hash         common.Hash   // 本块的hash
+	Height       uint64        // 本块的高度
 	PreHash      common.Hash   //上一块哈希
 	PreTime      time.Time     //上一块铸块时间
 	BlockHeight  uint64        //铸块高度
 	QueueNumber  uint32        //轮转序号
 	CurTime      time.Time     //当前铸块时间
 	Castor       groupsig.ID   //铸块人(ID同时决定了铸块人的权重)
+	Signature    common.Hash   // 组签名
 	Nonce        uint64        //盐
 	Transactions []common.Hash // 交易集哈希列表
 	TxTree       common.Hash   // 交易默克尔树根hash
@@ -24,14 +27,7 @@ type BlockHeader struct {
 	ExtraData    []int8
 }
 
-type SignedBlockHeader struct {
-	//签名
-	signature common.Hash
-	//区块头
-	header *BlockHeader
-}
-
 type Block struct {
-	header       *SignedBlockHeader
+	header       *BlockHeader
 	transactions []*Transaction
 }
