f567625ac61dbc0c1cdf31d89e33dff271d39854
achates <achates@taschain.io>
2018-04-15 00:22:30 +0800

    add encrypt/decrpt interface for publickey and privatekey

--- src/common/sign_test.go
+++ src/common/sign_test.go
@@ -2,10 +2,12 @@ package common
 
 import (
 	"crypto/sha1"
+	"crypto/rand"
 	"fmt"
 	"testing"
 
 	"golang.org/x/crypto/sha3"
+	"bytes"
 )
 
 func TestPrivateKey(test *testing.T) {
@@ -76,4 +78,42 @@ func TestSign(test *testing.T) {
 	success = pub_k.Verify(sha_buf, &sha3_si)
 	fmt.Printf("sha3 sign verify result=%v.\n", success)
 	fmt.Printf("end TestSign.\n")
+}
+
+
+func TestEncryptDecrypt(t *testing.T) {
+	fmt.Printf("\nbegin TestEncryptDecrypt...\n")
+	sk1 := GenerateKey("")
+	pk1 := sk1.GetPubKey()
+
+	sk2 := GenerateKey("")
+
+	message := []byte("Hello, world.")
+	ct, err := Encrypt(rand.Reader, &pk1, message)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	pt, err := sk1.Decrypt(rand.Reader, ct)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	fmt.Println(message)
+	fmt.Println(ct)
+	fmt.Println(pt)
+	
+	if !bytes.Equal(pt, message) {
+		fmt.Println("ecies: plaintext doesn't match message")
+		t.FailNow()
+	}
+
+	_, err = sk2.Decrypt(rand.Reader, ct)
+	if err == nil {
+		fmt.Println("ecies: encryption should not have succeeded")
+		t.FailNow()
+	}
+	fmt.Printf("end TestEncryptDecrypt.\n")
 }
