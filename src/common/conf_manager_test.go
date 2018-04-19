06a33d2026aa89fd7f869962d8be02be40adf9b7
问勤 <pxf@taschain.io>
2018-04-19 13:42:01 +0800

    confManager新增sectionmanager

--- src/common/conf_manager_test.go
+++ src/common/conf_manager_test.go
@@ -14,14 +14,20 @@ var (
 )
 
 func TestConfFileManager_SetBool(t *testing.T) {
-	//cm.SetBool("teSt_1", "bool_1", true)
-	//cm.SetDouble("test_2", "double_1", 10.33)
-	//cm.SetString("TTT", "STR", "abc好的")
 
-	t.Log(cm.GetBool("test_1", "netId"))
-	t.Log(cm.GetDouble("test_2", "double_1"))
-	t.Log(cm.GetString("test_2", "str1"))
-	t.Log(cm.GetString("test_2", "str2"))
-	t.Log(cm.GetString("ttt", "ID"))
+	cm.SetBool("teSt_1", "bool_1", true)
+	cm.SetDouble("test_2", "double_1", 10.33)
+	cm.SetString("TTT", "STR", "abc好的")
 
+	t.Log(cm.GetBool("test_1", "netId", true))
+	t.Log(cm.GetDouble("test_2", "double_1", 100))
+	t.Log(cm.GetString("test_2", "str1", "sss"))
+	t.Log(cm.GetString("test_2", "str2", "223"))
+	t.Log(cm.GetString("ttt", "ID", "dDDD"))
+
+	sm := cm.GetSectionManager("test_2")
+	t.Log(sm.GetString("str1", "sss"))
+	sm.SetDouble("d1", 2932)
+	sm.SetString("abc", "DBU")
+	sm.Del("str2")
 }
