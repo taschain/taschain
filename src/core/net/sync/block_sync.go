a9891b995fd5f76e4b77514e494263e9dda59a73
FlyingSmallBear <1581487855@qq.com>
2018-05-01 16:32:39 +0800

    去掉注释

--- src/core/net/sync/block_sync.go
+++ src/core/net/sync/block_sync.go
@@ -192,41 +192,41 @@ func marshalBlockMessage(e *core.BlockMessage) ([]byte, error) {
 	return proto.Marshal(&message)
 }
 
-func mockBlock() *core.Block {
-	txpool := core.BlockChainImpl.GetTransactionPool()
-	if nil == txpool {
-		logger.Error("fail to get txpool")
-	}
-
-	// 交易1
-	txpool.Add(genTestTx("tx1", 123, "111", "abc", 0, 1))
-
-	//交易2
-	txpool.Add(genTestTx("tx1", 456, "222", "ddd", 0, 1))
-
-	// 铸块1
-	block := core.BlockChainImpl.CastingBlock()
-	if nil == block {
-		logger.Error("fail to cast new block")
-	}
-	return block
-}
-
-func genTestTx(hash string, price uint64, source string, target string, nonce uint64, value uint64) *core.Transaction {
-
-	sourcebyte := common.BytesToAddress(core.Sha256([]byte(source)))
-	targetbyte := common.BytesToAddress(core.Sha256([]byte(target)))
-
-	//byte: 84,104,105,115,32,105,115,32,97,32,116,114,97,110,115,97,99,116,105,111,110
-	data := []byte("This is a transaction")
-	return &core.Transaction{
-		Data:     data,
-		Value:    value,
-		Nonce:    nonce,
-		Source:   &sourcebyte,
-		Target:   &targetbyte,
-		GasPrice: price,
-		GasLimit: 3,
-		Hash:     common.BytesToHash(core.Sha256([]byte(hash))),
-	}
-}
+//func mockBlock() *core.Block {
+//	txpool := core.BlockChainImpl.GetTransactionPool()
+//	if nil == txpool {
+//		logger.Error("fail to get txpool")
+//	}
+//
+//	// 交易1
+//	txpool.Add(genTestTx("tx1", 123, "111", "abc", 0, 1))
+//
+//	//交易2
+//	txpool.Add(genTestTx("tx1", 456, "222", "ddd", 0, 1))
+//
+//	// 铸块1
+//	block := core.BlockChainImpl.CastingBlock()
+//	if nil == block {
+//		logger.Error("fail to cast new block")
+//	}
+//	return block
+//}
+//
+//func genTestTx(hash string, price uint64, source string, target string, nonce uint64, value uint64) *core.Transaction {
+//
+//	sourcebyte := common.BytesToAddress(core.Sha256([]byte(source)))
+//	targetbyte := common.BytesToAddress(core.Sha256([]byte(target)))
+//
+//	//byte: 84,104,105,115,32,105,115,32,97,32,116,114,97,110,115,97,99,116,105,111,110
+//	data := []byte("This is a transaction")
+//	return &core.Transaction{
+//		Data:     data,
+//		Value:    value,
+//		Nonce:    nonce,
+//		Source:   &sourcebyte,
+//		Target:   &targetbyte,
+//		GasPrice: price,
+//		GasLimit: 3,
+//		Hash:     common.BytesToHash(core.Sha256([]byte(hash))),
+//	}
+//}
