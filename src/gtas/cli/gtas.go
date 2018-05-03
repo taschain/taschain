fd322d47a368b631209f556a87b3a4028e89f813
FlyingSmallBear <1581487855@qq.com>
2018-05-03 09:52:35 +0800

    测试

--- src/gtas/cli/gtas.go
+++ src/gtas/cli/gtas.go
@@ -74,7 +74,7 @@ func (gtas *Gtas) miner(rpc, super bool, rpcAddr string, rpcPort uint) {
 		keys := LoadPubKeyInfo()
 		fmt.Println("Waiting node to connect...")
 		for {
-			if len(p2p.Server.GetConnInfo()) >= 3 {
+			if len(p2p.Server.GetConnInfo()) >= 5 {
 				fmt.Println("Connection:")
 				for _, c := range p2p.Server.GetConnInfo() {
 					fmt.Println(c.Id)
