85af16d341aecac4fba61392322b177938d53ce9
FlyingSmallBear <1581487855@qq.com>
2018-06-07 10:22:03 +0800

    test

--- src/taslog/config.go
+++ src/taslog/config.go
@@ -2,7 +2,7 @@ package taslog
 
 
 const(
- DefaultConfig = `<seelog minlevel="debug">
+ DefaultConfig = `<seelog minlevel="error">
 						<outputs formatid="default">
 							<rollingfile type="size" filename="./logs/default.log" maxsize="500000000" maxrolls="10"/>
 						</outputs>
@@ -11,7 +11,7 @@ const(
 						</formats>
 					</seelog>`
 
- P2PConfig = `<seelog minlevel="debug">
+ P2PConfig = `<seelog minlevel="error">
 						<outputs formatid="default">
 							<rollingfile type="size" filename="./logs/p2p.log" maxsize="500000000" maxrolls="10"/>
 						</outputs>
