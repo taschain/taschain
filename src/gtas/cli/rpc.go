62b01bf71f1cb117598f1df1cdf01bf0d01d2eb6
问勤 <pxf@taschain.io>
2018-06-07 15:57:00 +0800

    1. globalTicker优化支持立即触发; 2. OMC和startCast可能并发到来导致FindSlotByQN为nil

--- src/gtas/cli/rpc.go
+++ src/gtas/cli/rpc.go
@@ -11,6 +11,7 @@ import (
 	"encoding/hex"
 	"strings"
 	"log"
+	"math"
 )
 
 // GtasAPI is a single-method API handler to be returned by test services.
@@ -133,7 +134,7 @@ func (api *GtasAPI) GetBlock(height uint64) (*Result, error) {
 	blockDetail["group_id"] = hex.EncodeToString(bh.GroupId)
 	blockDetail["signature"] = hex.EncodeToString(bh.Signature)
 	blockDetail["txs"] = len(bh.Transactions)
-	blockDetail["tps"] = len(bh.Transactions)/int(bh.CurTime.Sub(bh.PreTime).Seconds())
+	blockDetail["tps"] = math.Round(float64(len(bh.Transactions))/bh.CurTime.Sub(bh.PreTime).Seconds())
 	return &Result{"success", blockDetail}, nil
 }
 
