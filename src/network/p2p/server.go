deec1426c536413bf1a23e100661d3671e9bd18c
FlyingSmallBear <1581487855@qq.com>
2018-06-26 17:57:35 +0800

    server 修改1

--- src/network/p2p/server.go
+++ src/network/p2p/server.go
@@ -103,7 +103,7 @@ type server struct {
 func InitServer(host host.Host, dht *dht.IpfsDHT, node *Node) {
 	logger = taslog.GetLoggerByName("p2p" + common.GlobalConf.GetString("client", "index", ""))
 	host.SetStreamHandler(ProtocolTAS, swarmStreamHandler)
-	Server = server{Host: host, Dht: dht, SelfNetInfo: node, streams: make(map[string]inet.Stream)}
+	Server = server{Host: host, Dht: dht, SelfNetInfo: node, streams: make(map[string]inet.Stream),streamWriters: make(map[string]*bufio.Writer)}
 }
 
 func (s *server) SendMessage(m Message, id string) {
