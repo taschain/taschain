684812ccfa08a565aba827ad6c6b77087a24b902
tequila <114778836@qq.com>
2018-07-20 10:17:50 +0800

    publish/subscribe

---
+++ src/middleware/notify/bus.go
@@ -0,0 +1,57 @@
+package notify
+
+import (
+	"sync"
+)
+
+type Bus struct {
+	topics map[string]Topic
+	lock   sync.RWMutex
+}
+
+func NewBus() *Bus {
+	return &Bus{
+		lock:   sync.RWMutex{},
+		topics: make(map[string]Topic, 10),
+	}
+}
+
+func (bus *Bus) Subscribe(id string, handler Handler) {
+	bus.lock.Lock()
+	defer bus.lock.Unlock()
+
+	topic, ok := bus.topics[id]
+	if !ok {
+		topic = Topic{
+			Id: id,
+		}
+		bus.topics[id] = topic
+	}
+
+	topic.Subscribe(handler)
+}
+
+func (bus *Bus) UnSubscribe(id string, handler Handler) {
+	bus.lock.RLock()
+	defer bus.lock.RUnlock()
+
+	topic, ok := bus.topics[id]
+	if !ok {
+		return
+	}
+
+	topic.UnSubscribe(handler)
+}
+
+func (bus *Bus) Publish(id string, message Message) {
+	bus.lock.RLock()
+	defer bus.lock.RUnlock()
+
+	topic, ok := bus.topics[id]
+	if !ok {
+		return
+	}
+
+	topic.Handle(message)
+}
+
---
+++ src/middleware/notify/topic.go
@@ -0,0 +1,61 @@
+package notify
+
+import "sync"
+
+type Message interface {
+	GetRaw() []byte
+	GetData() struct{}
+}
+
+type DummyMessage struct {
+}
+func (d *DummyMessage) GetRaw() []byte {
+	return []byte{}
+}
+func (d *DummyMessage) GetData() struct{} {
+	return struct{}{}
+}
+
+
+type Handler interface {
+	// 处理消息
+	Handle(message Message)
+}
+
+// 消息订阅
+type Topic struct {
+	Id       string
+	handlers []Handler
+	lock     sync.RWMutex
+}
+
+func (topic *Topic) Subscribe(h Handler) {
+	topic.lock.Lock()
+	defer topic.lock.Unlock()
+
+	topic.handlers = append(topic.handlers, h)
+}
+
+func (topic *Topic) UnSubscribe(h Handler) {
+	topic.lock.Lock()
+	defer topic.lock.Unlock()
+
+	for i, handler := range topic.handlers {
+		if handler == h {
+			topic.handlers = append(topic.handlers[:i], topic.handlers[i+1:]...)
+			return
+		}
+	}
+}
+
+func (topic *Topic) Handle(message Message) {
+	if 0 == len(topic.handlers) {
+		return
+	}
+
+	topic.lock.RLock()
+	defer topic.lock.RUnlock()
+	for _, h := range topic.handlers {
+		go h.Handle(message)
+	}
+}
