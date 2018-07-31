ca32be59d6694b602a738e4ef09b9101ebca8c6f
ysh0566 <ysh0566@qq.com>
2018-07-31 17:13:44 +0800

    ""

--- src/tvm/py/token.py
+++ src/tvm/py/token.py
@@ -1,161 +1,49 @@
-package tvm
-
-import (
-	"io/ioutil"
-	"fmt"
-)
-
-func Read0(filename string)  (string){
-	f, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println("read fail", err)
-	}
-	return string(f)
-}
-
-func VmTest() {
-
-	tvm := NewTvm(nil)
-	tvm.Execute(Read0("py/token.py"))
-}
-
-func VmTestContract() {
-	tvm := NewTvm(nil)
-
-	script := `
-import tas
-class TasAccount():
-
-    address = ""
-
-    def transfer(self, toAddress, amount):
-       tas.transfer(self.address, toAddress, amount)
-`
-
-	tvm.Execute(script)
-
-	script = `
-
-def apply():
-    myAccount = TasAccount()
-    myAccount.address = "myAddress"
-    otherAccount = "otherAddress"
-    myAccount.transfer(otherAccount, 50)
-
-apply()
-`
-	tvm.Execute(script)
-}
-
-func VmTestClass() {
-	tvm := NewTvm(nil)
-
-	script := `
-
-from tas import *
-
-test()
-
-tasa = tasaccount()
-
-print(tasa)
-
-#print(tasa.hello())
-
-print("start")
-
-print(tasa.desc)
-
-tasa.desc = 123
-
-print(tasa.desc)
-
-print("end")
-
-`
-	tvm.Execute(script)
-}
-
-func VmTestABI() {
-	tvm := NewTvm(nil)
-
-
-	tvm.Execute(`
-def Test(a, b, c, d):
-    print(a)
-    print(b)
-    print(c)
-    print(d)
-`)
-
-	str := `{"FuncName": "Test", "Args": [10.123, "ten", [1, 2], {"key":"value", "key2":"value2"}]}`
-	tvm.ExecuteABIJson(str)
-}
-
-func VmTestException() {
-	tvm := NewTvm(nil)
-
-	tvm.Execute(`
-i am error
-`)
-}
-
-func VmTestToken(){
-	tvm := NewTvm(nil)
-
-	tvm.Execute(`
-import account
 class Storage(object):
     data = {}
     @staticmethod
     def get(key):
-        return data[key]
+        return Storage.data[key]
 
     @staticmethod
     def put(key, value):
-        data[key] = value
+        Storage.data[key] = value
 
     @staticmethod
     def delete(key):
-        del data[key]
+        del Storage.data[key]
 
+    @staticmethod
+    def load(obj):
+        for k in Storage.data:
+            obj.__dict__[k] = Storage.data[k]
+
+    @staticmethod
+    def save(obj):
+        for k in obj.__dict__:
+            Storage.put(k, obj.__dict__[k])
 
 def require(b):
     if not b:
         raise (Exception, "")
 
 
-class Address(object):
-    def __str__(self):
-        return self.value
-
-    def __set__(self, instance, value):
-        self.value = value
-
-    def __init__(self, value):
-        self.value = value
-
-    def __eq__(self, other):
-        return self.value == other.value
-
+class Address(str):
     def invalid(self):
         # TODO 检查是否合法地址
         return True
 
     def balance(self):
-        #获取地址里的余额
-		leftSum = account.getBalance(self.value)
-        return leftSum
+        # TODO 获取地址里的余额
+        return 0
 
     def transfer(self, _value):
-        #转账到合约
-		account.transfer(self.value, this, _value )
+        # TODO 转账到合约
         pass
 
 
-# 当前合约的地址
-this = account.contractAddr(Msg.sender);
 
+this = Address("")
+owner = Address("")
 
 class Msg(object):
     sender = Address("")
@@ -163,12 +51,11 @@ class Msg(object):
 
 
 #调用者是否为合约创建者
-def owner():
-    b = account.isContractCreater(Msg.sender)
-    if b:
+def check_owner():
+    if owner == Msg.sender:
         return True
     else:
-        raise (Exception, "只有合约owner可以操作")
+        raise Exception("只有合约owner可以操作")
 
 
 class Event(object):
@@ -181,6 +68,20 @@ class Event(object):
 #
 #
 
+class BalanceDict():
+    def __init__(self):
+        self.data = {}
+
+    def __getitem__(self, item):
+        if item not in self.data:
+            self.data[item] = 0
+        return self.data[item]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
 
 class TokenERC20(object):
     def __init__(self):
@@ -188,7 +89,7 @@ class TokenERC20(object):
         self.symbol = ""
         self.totalSupply = 0
 
-        self.balanceOf = {}
+        self.balanceOf = BalanceDict()
         self.allowance = {}
     '''
     // This generates a public event on the blockchain that will notify clients
@@ -202,10 +103,10 @@ class TokenERC20(object):
     '''
 
     def _transfer(self, _from, _to, _value):
-        if _to not in self.balanceOf:
-            self.balanceOf[_to] = 0
-        if _from not in self.balanceOf:
-            self.balanceOf[_from] = 0
+        # if _to not in self.balanceOf:
+        #     self.balanceOf[_to] = 0
+        # if _from not in self.balanceOf:
+        #     self.balanceOf[_from] = 0
         # 接收账户地址是否合法
         require(_to.invalid())
         # 账户余额是否满足转账金额
@@ -247,8 +148,8 @@ class TokenERC20(object):
         return True
 
     def burnFrom(self, _from, _value):
-        if _from not in self.balanceOf:
-            self.balanceOf[_from] = 0
+        # if _from not in self.balanceOf:
+        #     self.balanceOf[_from] = 0
         #检查账户余额
         require(self.balanceOf[_from] >= _value)
         require(_value <= self.allowance[_from][Msg.sender])
@@ -278,6 +179,7 @@ class MyAdvancedToken(TokenERC20):
         self.name = "TAS"
         self.symbol = "%"
         self.totalSupply = 1000000
+        self.balanceOf[Msg.sender] = self.totalSupply
 
     #/* This generates a public event on the blockchain that will notify clients */
     #event FrozenFunds(address target, bool frozen);
@@ -301,34 +203,35 @@ class MyAdvancedToken(TokenERC20):
         require(_to.invalid)
         require(self.balanceOf[_from] >= _value)
         require(_value > 0)
-        require(not self.frozenAccount[_from])
-        require(not self.frozenAccount[_to])
+        # require((_from not in self.frozenAccount) or (not self.frozenAccount[_from]))
+        # require((_to not in self.frozenAccount) or (not self.frozenAccount[_to]))
         self.balanceOf[_from] -= _value
+        # if _to not in self.balanceOf:
+        #     self.balanceOf[_to] = 0
         self.balanceOf[_to] += _value
         Event.emit("Transfer", _from, _to, _value)
 
     def mint_token(self, target, minted_amount):
-        owner()
+        check_owner()
         self.balanceOf[target] += minted_amount
         self.totalSupply += minted_amount
         Event.emit("Transfer", 0, this, minted_amount)
         Event.emit("Transfer", this, target, minted_amount)
 
     def freeze_account(self, target, freeze):
-        owner()
+        check_owner()
         self.frozenAccount[target] = freeze
         Event.emit("FrozenFunds", target, freeze)
 
     def set_prices(self, new_sell_price, new_buy_price):
-        owner()
+        check_owner()
         self.sell_price = new_sell_price
         self.buy_price = new_buy_price
 
     def buy(self):
         amount = Msg.value / self.buy_price
         self._transfer(this, Msg.sender, amount)
-        #扣钱
-		account.subBlance(Msg.sender, amount)
+        # TODO 扣钱
 
     def sell(self, amount):
         require(this.balance() >= amount * self.sell_price)
@@ -340,5 +243,41 @@ class MyAdvancedToken(TokenERC20):
 #
 #
 #
-`)
-}
+
+if __name__ == '__main__':
+    Msg.sender = Address("0xabcdefghijk")
+    Msg.value = 0
+
+    # 部署合约
+    myAdvancedToken = MyAdvancedToken()
+    myAdvancedToken.deploy()
+    Storage.save(myAdvancedToken)
+
+    # 执行合约
+    # [1] 初始化测试环境
+    this = Address("0x123456789")
+    owner = Address("0xabcdefghijk")
+    # [2] 执行合约
+
+    # Test 1
+    myAdvancedToken = MyAdvancedToken()
+    Storage.load(myAdvancedToken)
+    myAdvancedToken.set_prices(100, 100)
+    Storage.save(myAdvancedToken)
+    # Test 2
+    myAdvancedToken = MyAdvancedToken()
+    Storage.load(myAdvancedToken)
+    myAdvancedToken.transfer(Address("0xbcbcbcbcbc"), 50)
+    Storage.save(myAdvancedToken)
+    print(myAdvancedToken.balanceOf.data)
+
+
+
+
+
+
+
+
+
+
+
