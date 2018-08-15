63676965930c5ec2c24623ddc5b73adcb1527393
FlyingSmallBear <1581487855@qq.com>
2018-08-15 10:46:22 +0800

    启动redis 隔离变更： 脚本kill -9 改为kill -3 super节点启动清理REDIS数据  逻辑删除

--- src/gtas/cli/gtas.go
+++ src/gtas/cli/gtas.go
@@ -283,11 +283,6 @@ func (gtas *Gtas) fullInit(isSuper, testMode bool, seedIp string) error {
 		return errors.New("gov module error")
 	}
 
-	if isSuper {
-		//超级节点启动前先把Redis数据清空
-		redis.CleanRedisData()
-	}
-
 	secret := (*configManager).GetString(Section, "secret", "")
 	if secret == "" {
 		secret = getRandomString(5)
