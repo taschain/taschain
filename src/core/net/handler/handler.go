3e5ef39ebd29d3f42446bfc19957226b80dc3bdb
FlyingSmallBear <1581487855@qq.com>
2018-08-28 16:41:29 +0800

    精简日志

--- src/core/net/handler/handler.go
+++ src/core/net/handler/handler.go
@@ -140,7 +140,7 @@ func (c *ChainHandler) Handle(sourceId string, msg network.Message) error {
 }
 
 func (ch ChainHandler) newBlockHeaderHandler(msg notify.Message) {
-	core.Logger.Debugf("[ChainHandler]newBlockHeaderHandler RCV MSG")
+	//core.Logger.Debugf("[ChainHandler]newBlockHeaderHandler RCV MSG")
 	m, ok := msg.GetData().(*notify.BlockHeaderNotifyMessage)
 	if !ok {
 		core.Logger.Debugf("[ChainHandler]newBlockHeaderHandler GetData assert not ok!")
@@ -158,7 +158,7 @@ func (ch ChainHandler) newBlockHeaderHandler(msg notify.Message) {
 }
 
 func (ch ChainHandler) blockBodyReqHandler(msg notify.Message) {
-	core.Logger.Debugf("[ChainHandler]blockBodyReqHandler RCV MSG")
+	//core.Logger.Debugf("[ChainHandler]blockBodyReqHandler RCV MSG")
 	m, ok := msg.GetData().(*notify.BlockBodyReqMessage)
 	if !ok {
 		core.Logger.Debugf("[ChainHandler]blockBodyReqHandler GetData assert not ok!")
@@ -173,7 +173,7 @@ func (ch ChainHandler) blockBodyReqHandler(msg notify.Message) {
 }
 
 func (ch ChainHandler) blockBodyHandler(msg notify.Message) {
-	core.Logger.Debugf("[ChainHandler]blockBodyHandler RCV MSG")
+	//core.Logger.Debugf("[ChainHandler]blockBodyHandler RCV MSG")
 	m, ok := msg.GetData().(*notify.BlockBodyNotifyMessage)
 	if !ok {
 		return
@@ -193,7 +193,7 @@ func (ch ChainHandler) loop() {
 	for {
 		select {
 		case headerNotify := <-ch.headerCh:
-			core.Logger.Debugf("[ChainHandler]headerCh receive,hash:%v,peer:%s",headerNotify.header.Hash,headerNotify.peer)
+			//core.Logger.Debugf("[ChainHandler]headerCh receive,hash:%v,peer:%s",headerNotify.header.Hash,headerNotify.peer)
 			hash := headerNotify.header.Hash
 			if _, ok := ch.headerPending[hash]; ok || ch.complete.Contains(hash) {
 				core.Logger.Debugf("[ChainHandler]header hit pending or complete")
@@ -209,7 +209,7 @@ func (ch ChainHandler) loop() {
 			ch.headerPending[hash] = headerNotify
 			core.ReqBlockBody(headerNotify.peer, hash)
 		case bodyNotify := <-ch.bodyCh:
-			core.Logger.Debugf("[ChainHandler]bodyCh receive,hash:%v,peer:%s",bodyNotify.blockHash,bodyNotify.peer)
+			//core.Logger.Debugf("[ChainHandler]bodyCh receive,hash:%v,peer:%s",bodyNotify.blockHash,bodyNotify.peer)
 			headerNotify, ok := ch.headerPending[bodyNotify.blockHash]
 			if !ok {
 				break
@@ -291,7 +291,7 @@ func onBlockHashes(bhs []*core.BlockHash, sourceId string) {
 
 func onBlockInfoReq(erm core.BlockRequestInfo, sourceId string) {
 	//收到块请求
-	core.Logger.Debugf("[handler]onBlockInfoReq get message from:%s", sourceId)
+	//core.Logger.Debugf("[handler]onBlockInfoReq get message from:%s", sourceId)
 	if nil == core.BlockChainImpl {
 		return
 	}
@@ -301,13 +301,13 @@ func onBlockInfoReq(erm core.BlockRequestInfo, sourceId string) {
 
 func onBlockInfo(blockInfo core.BlockInfo, sourceId string) {
 	//收到块信息
-	core.Logger.Debugf("[handler] onBlockInfo get message from:%s", sourceId)
+	//core.Logger.Debugf("[handler] onBlockInfo get message from:%s", sourceId)
 	if nil == core.BlockChainImpl {
 		return
 	}
 	block := blockInfo.Block
 	if block != nil {
-		core.Logger.Debugf("[handler] onBlockInfo receive block,height:%d,qn:%d",block.Header.Height,block.Header.QueueNumber)
+		//core.Logger.Debugf("[handler] onBlockInfo receive block,height:%d,qn:%d",block.Header.Height,block.Header.QueueNumber)
 		code := core.BlockChainImpl.AddBlockOnChain(block)
 		if code < 0 {
 			core.BlockChainImpl.SetAdujsting(false)
@@ -328,7 +328,7 @@ func onBlockInfo(blockInfo core.BlockInfo, sourceId string) {
 			}
 		}
 	} else {
-		core.Logger.Debugf("[handler] onBlockInfo receive chainPiece,length:%d", len(blockInfo.ChainPiece))
+		//core.Logger.Debugf("[handler] onBlockInfo receive chainPiece,length:%d", len(blockInfo.ChainPiece))
 		chainPiece := blockInfo.ChainPiece
 		core.BlockChainImpl.CompareChainPiece(chainPiece, sourceId)
 	}
