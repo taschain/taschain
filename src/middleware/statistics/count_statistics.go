b42318c55d580c3fd4a3ee5e3a9451e0b35d6092
风洺 <kaede.yh@alibaba-inc.com>
2018-08-29 15:05:23 +0800

    add count

--- src/middleware/statistics/count_statistics.go
+++ src/middleware/statistics/count_statistics.go
@@ -9,18 +9,51 @@ import (
 	"sync"
 )
 
-var count_map = make(map[string]*sync.Map)
+type countItem struct {
+	lock *sync.RWMutex
+	innerMap *sync.Map
+}
+
+var count_map = make(map[string]*countItem)
 var logger taslog.Logger
 
+func newCountItem() *countItem {
+	return &countItem{lock:new(sync.RWMutex),innerMap:new(sync.Map)}
+}
+
+func (item *countItem)get(code uint32) uint32 {
+	item.lock.RLock()
+	defer item.lock.RUnlock()
+	if v,ok2 := item.innerMap.Load(code);ok2{
+		return v.(uint32)
+	} else{
+		return 0
+	}
+}
+
+func (item *countItem)set(code uint32,value uint32) {
+	item.lock.RLock()
+	defer item.lock.RUnlock()
+	item.innerMap.Store(code, value)
+}
+
+func (item *countItem)print() string{
+	item.lock.Lock()
+	defer item.lock.Unlock()
+	var buffer bytes.Buffer
+	item.innerMap.Range(func(code, value interface{}) bool {
+		buffer.WriteString(fmt.Sprintf(" %d:%d",code,value))
+		item.innerMap.Delete(code)
+		return true
+	})
+	return buffer.String()
+}
+
 func printAndRefresh()  {
-	for key,vmap := range count_map{
-		count_map[key] = &sync.Map{}
-		var buffer bytes.Buffer
-		vmap.Range(func(code, value interface{}) bool {
-			buffer.WriteString(fmt.Sprintf(" %d:%d",code,value))
-			return true
-		})
-		logger.Infof("%s%s\n", key, buffer.String())
+	for key,item := range count_map{
+		content := item.print()
+		logger.Infof("%s%s\n", key, content)
+		//fmt.Printf("%s%s\n", key, content)
 		//if len(vmap) > 0{
 		//	pmap := vmap
 		//	count_map[key] = make(map[uint32]uint32)
@@ -35,16 +68,13 @@ func printAndRefresh()  {
 }
 
 func AddCount(name string, code uint32)  {
-	if vmap,ok := count_map[name];ok{
-		if v,ok2 := vmap.Load(code);ok2{
-			vmap.Store(code,v.(uint32) + uint32(1))
-		} else {
-			vmap.Store(code,uint32(1))
-		}
+	if item,ok := count_map[name];ok{
+		newValue := item.get(code) + 1
+		item.set(code, newValue)
 	} else {
-		vmap = &sync.Map{}
-		vmap.Store(code,uint32(1))
-		count_map[name] = vmap
+		item = newCountItem()
+		item.set(code, 1)
+		count_map[name] = item
 	}
 	//logger.Infof("%s %d",name,code)
 }
