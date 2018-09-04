09cdc43a464d3a337439c762a48e876aecf22a2f
FlyingSmallBear <1581487855@qq.com>
2018-09-04 10:00:14 +0800

    1.p2p_core链接库更新 2.增加命令行启动参数

--- deploy/pre/start.sh
+++ deploy/pre/start.sh
@@ -2,7 +2,7 @@
 instance_index=$1
 instance_count=$2
 instance_end=$instance_index+$instance_count
-
+nat_server=$3
 
 for((;instance_index<instance_end;instance_index++))
 
@@ -28,9 +28,9 @@ do
 	#echo -e 'nohup ./gtas miner --config' $config_file '--rpc --rpcport' $rpc_port '--super --instance' $instance_index '--pprof' $pprof_port '>' $stdout_log '2>&1 & echo $! >' $pid_file
 
 	if [ $instance_index -eq 1 ];then
-		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --prefix aly_flow --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --prefix aly_flow --nat $nat_server --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
 	else
-		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --prefix aly_flow --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --prefix aly_flow --nat $nat_server --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
 	fi
 	sleep 0.5
 done
