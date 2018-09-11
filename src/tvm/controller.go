b28b7dd01b573c101ea31dc95331f45399cdd1ae
tequila <114778836@qq.com>
2018-09-11 11:40:39 +0800

    https://github.com/taschain/taschain/issues/4 处理调用合约有value的情况

--- src/tvm/controller.go
+++ src/tvm/controller.go
@@ -1,20 +1,22 @@
 package tvm
 
 import (
-	"fmt"
 	"storage/core/vm"
 	"middleware/types"
+	"fmt"
 	"common"
+	"math/big"
+	"core"
 )
 
 type Controller struct {
 	BlockHeader *types.BlockHeader
 	Transaction *types.Transaction
-	AccountDB vm.AccountDB
-	Reader vm.ChainReader
-	Vm *Tvm
-	Tasks []*CallTask
-	LibPath string
+	AccountDB   vm.AccountDB
+	Reader      vm.ChainReader
+	Vm          *Tvm
+	Tasks       []*CallTask
+	LibPath     string
 }
 
 func NewController(accountDB vm.AccountDB,
@@ -34,12 +36,11 @@ func NewController(accountDB vm.AccountDB,
 	return controller
 }
 
-
-func(con *Controller) Deploy(sender *common.Address, contract *Contract) bool{
+func (con *Controller) Deploy(sender *common.Address, contract *Contract) bool {
 	var succeed bool
 	con.Vm = NewTvm(sender, contract, con.LibPath)
 	con.Vm.SetGas(int(con.Transaction.GasLimit))
-	msg := Msg{Data:[]byte{}, Value:con.Transaction.Value, Sender: con.Transaction.Source.GetHexString()}
+	msg := Msg{Data: []byte{}, Value: con.Transaction.Value, Sender: con.Transaction.Source.GetHexString()}
 	snapshot := con.AccountDB.Snapshot()
 	succeed = con.Vm.Deploy(msg) && con.Vm.StoreData()
 	if !succeed {
@@ -50,26 +51,38 @@ func(con *Controller) Deploy(sender *common.Address, contract *Contract) bool{
 	return succeed
 }
 
-func(con *Controller) ExecuteAbi(sender *common.Address, contract *Contract, abi string) bool {
+func (con *Controller) ExecuteAbi(sender *common.Address, contract *Contract, abi string) bool {
 	var succeed bool
-	con.Vm  = NewTvm(sender, contract, con.LibPath)
+	con.Vm = NewTvm(sender, contract, con.LibPath)
 	con.Vm.SetGas(1000000)
-	snapshot := con.AccountDB.Snapshot()
-	msg := Msg{Data:con.Transaction.Data, Value:con.Transaction.Value, Sender: con.Transaction.Source.GetHexString()}
+
+	//先转账
+	if con.Transaction.Value > 0 {
+		amount := big.NewInt(int64(con.Transaction.Value))
+		if core.CanTransfer(con.AccountDB, *sender, amount) {
+			core.Transfer(con.AccountDB, *sender, *con.Transaction.Target, amount)
+		} else {
+			return false
+		}
+	}
+
+	msg := Msg{Data: con.Transaction.Data, Value: con.Transaction.Value, Sender: con.Transaction.Source.GetHexString()}
 	succeed = con.Vm.LoadContractCode()
 	if succeed {
 		con.Vm.SetGas(int(con.Transaction.GasLimit))
 		succeed = con.Vm.ExecuteABIJson(msg, abi) && con.Vm.StoreData()
 	}
-	if !succeed {
-		con.AccountDB.RevertToSnapshot(snapshot)
-	}
+
 	con.Vm.DelTvm()
 	con.ExecuteTask()
 	return succeed
 }
 
-func(con *Controller) ExecuteTask() {
+//func CanTransfer(db vm.AccountDB, addr common.Address, amount *big.Int) bool {
+//	return db.GetBalance(addr).Cmp(amount) >= 0
+//}
+
+func (con *Controller) ExecuteTask() {
 	var succeed bool
 	for _, task := range con.Tasks {
 		contract := LoadContract(*task.ContractAddr)
@@ -77,7 +90,7 @@ func(con *Controller) ExecuteTask() {
 		con.Vm = NewTvm(task.Sender, contract, con.LibPath)
 		con.Vm.SetGas(1000000)
 		snapshot := con.AccountDB.Snapshot()
-		msg := Msg{Data:[]byte{}, Value:0, Sender: task.Sender.GetHexString()}
+		msg := Msg{Data: []byte{}, Value: 0, Sender: task.Sender.GetHexString()}
 		abi := fmt.Sprintf(`{"FuncName": "%s", "Args": %s}`, task.FuncName, task.Params)
 		succeed = con.Vm.LoadContractCode()
 		if succeed {
