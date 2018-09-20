b404eda21b4b231dcb0c37570a3cfed941b8ccb0
ysh0566 <ysh0566@qq.com>
2018-09-20 17:27:54 +0800

    去除交易放大器

--- src/gtas/cli/wallets.go
+++ src/gtas/cli/wallets.go
@@ -46,16 +46,15 @@ func (ws *wallets) transaction(source, target string, value uint64, code string,
 	var transaction *types.Transaction
 	var contractAddr common.Address
 	var i uint64 = 0
-	for ; i < 100; i++ {
-		transaction = genTx(0, source, target, nonce+i, value, []byte(code), nil, 0)
-		transaction.Hash = transaction.GenHash()
-		_, err := txpool.Add(transaction)
-		if err != nil {
-			return nil, nil, err
-		}
-		if code != "" {
-			contractAddr = common.BytesToAddress(common.Sha256(common.BytesCombine(transaction.Source[:], common.Uint64ToByte(nonce))))
-		}
+	transaction = genTx(0, source, target, nonce+i, value, []byte(code), nil, 0)
+	transaction.Hash = transaction.GenHash()
+	_, err := txpool.Add(transaction)
+	if err != nil {
+		return nil, nil, err
+	}
+	if code != "" {
+		lastNonce := core.BlockChainImpl.GetNonce(common.BytesToAddress(transaction.Source[:]))
+		contractAddr = common.BytesToAddress(common.Sha256(common.BytesCombine(transaction.Source[:], common.Uint64ToByte(lastNonce))))
 	}
 
 
