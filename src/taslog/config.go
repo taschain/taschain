ecdb46dd4d41be0c417591a76d602a3aa35bb6e9
lavrock <freewenjie@gmail.com>
2018-10-15 18:03:47 +0800

    修改日志配置

--- src/taslog/config.go
+++ src/taslog/config.go
@@ -28,7 +28,7 @@ const(
 
  P2PConfig = `<seelog minlevel="debug">
 						<outputs formatid="default">
-							<rollingfile type="size" filename="./logs/p2p.log" maxsize="500000000" maxrolls="10"/>
+							<rollingfile type="size" filename="./logs/p2p.log" maxsize="256000000" maxrolls="2"/>
 						</outputs>
 						<formats>
 							<format id="default" format="%Date(2006-01-02 15:04:05.000)  [%Level]  [%File:%Line] %Msg%n" />
