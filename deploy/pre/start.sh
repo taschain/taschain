f54e644e83c155492a2c444820bfc310ec88158d
风洺 <kaede.yh@alibaba-inc.com>
2018-10-26 10:28:32 +0800

    pre start add apply

--- deploy/pre/start.sh
+++ deploy/pre/start.sh
@@ -4,6 +4,7 @@ instance_count=$2
 instance_end=$instance_index+$instance_count
 nat_server=$3
 build_number=$4
+apply=$5
 
 for((;instance_index<instance_end;instance_index++))
 
@@ -29,8 +30,8 @@ do
 	#echo -e 'nohup ./gtas miner --config' $config_file '--rpc --rpcport' $rpc_port '--super --instance' $instance_index '--pprof' $pprof_port '>' $stdout_log '2>&1 & echo $! >' $pid_file
 
 	if [ $instance_index -eq 1 ];then
-		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --prefix aly_flow --nat $nat_server --build_id $build_number --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --prefix aly_flow --nat $nat_server --build_id $build_number --pprof $pprof_port --apply $apply > $stdout_log 2>&1 & echo $! > $pid_file
 	else
-		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --prefix aly_flow --nat $nat_server --build_id $build_number --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --prefix aly_flow --nat $nat_server --build_id $build_number --pprof $pprof_port --apply $apply > $stdout_log 2>&1 & echo $! > $pid_file
 	fi
 done
