0f89af6292bd2588bfba1f00e689b3ee3ceac3b0
tequila <114778836@qq.com>
2018-10-30 16:25:09 +0800

    增加copyright信息

--- src/storage/trie/light_trie_test.go
+++ src/storage/trie/light_trie_test.go
@@ -1,9 +1,24 @@
+//   Copyright (C) 2018 TASChain
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package trie
 
 import (
 	"testing"
 	"common"
-	"core/datasource"
+
 	"storage/tasdb"
 )
 
@@ -41,7 +56,7 @@ func TestLightInsertAndDel(t *testing.T) {
 
 
 func TestLRU(t *testing.T) {
-	db, _ := datasource.NewLRUMemDatabase(10)
+	db, _ := tasdb.NewLRUMemDatabase(10)
 	triedb := NewDatabase(db)
 	trie,_:=NewLightTrie(common.Hash{}, triedb)
 	updateLightString(trie,"key1","1")
