2a78369202af6319dbf22273692ccabaece75516
lavrock <freewenjie@gmail.com>
2018-11-19 19:01:02 +0800

    发送数据按添加code类型统计功能

--- src/network/peer.go
+++ src/network/peer.go
@@ -99,10 +99,12 @@ func (p *Peer) isEmpty() bool {
 	return empty
 }
 
-func (p *Peer) write(packet *bytes.Buffer) {
+func (p *Peer) write(packet *bytes.Buffer,code uint32) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 	P2PSend(p.seesionId, packet.Bytes())
+	netCore.flowMeter.send(int64(code), int64(len(packet.Bytes())))
+
 }
 
 func (p *Peer) getDataSize() int {
@@ -133,7 +135,7 @@ func newPeerManager() *PeerManager {
 	return pm
 }
 
-func (pm *PeerManager) write(toid NodeID, toaddr *nnet.UDPAddr, packet *bytes.Buffer) error {
+func (pm *PeerManager) write(toid NodeID, toaddr *nnet.UDPAddr, packet *bytes.Buffer,code uint32) error {
 
 	netId := netCoreNodeID(toid)
 	p := pm.peerByNetID(netId)
@@ -147,9 +149,8 @@ func (pm *PeerManager) write(toid NodeID, toaddr *nnet.UDPAddr, packet *bytes.Bu
 
 	Logger.Debugf("write Id:%v netid:%v session:%v size %v", toid.GetHexString(), netId, p.seesionId, len(packet.Bytes()))
 
-
 	if p.seesionId > 0 {
-		p.write(packet)
+		p.write(packet,code)
 	} else {
 
 		if ((toaddr != nil && toaddr.IP != nil && toaddr.Port > 0) || pm.natTraversalEnable) && !p.connecting {
@@ -179,8 +180,6 @@ func (pm *PeerManager) write(toid NodeID, toaddr *nnet.UDPAddr, packet *bytes.Bu
 		}
 	}
 
-	netCore.flowMeter.send(0, int64(len(packet.Bytes())))
-
 	return nil
 }
 
@@ -202,7 +201,7 @@ func (pm *PeerManager) newConnection(id uint64, session uint32, p2pType uint32, 
 
 	for e := p.sendList.Front(); e != nil; e = e.Next() {
 		buf := e.Value.(*bytes.Buffer)
-		p.write(buf)
+		p.write(buf,0)
 	}
 	p.sendList = list.New()
 	Logger.Debugf("newConnection node id:%v  netid :%v session:%v isAccepted:%v ", p.Id.GetHexString(), id, session, isAccepted)
@@ -246,7 +245,7 @@ func (pm *PeerManager) OnChecked(p2pType uint32, privateIp string, publicIp stri
 }
 
 //SendDataToAll 向所有已经连接的节点发送自定义数据包
-func (pm *PeerManager) SendAll(packet *bytes.Buffer) {
+func (pm *PeerManager) SendAll(packet *bytes.Buffer ,code uint32) {
 	pm.mutex.RLock()
 	defer pm.mutex.RUnlock()
 	//pm.checkPeerSource()
@@ -257,7 +256,7 @@ func (pm *PeerManager) SendAll(packet *bytes.Buffer) {
 		//	p.write(packet)
 		//}
 		if p.seesionId > 0 {
-			p.write(packet)
+			p.write(packet,code)
 		}
 	}
 
@@ -278,7 +277,7 @@ func (pm *PeerManager) checkPeerSource() {
 }
 
 //BroadcastRandom
-func (pm *PeerManager) BroadcastRandom(packet *bytes.Buffer) {
+func (pm *PeerManager) BroadcastRandom(packet *bytes.Buffer,code uint32) {
 	pm.mutex.RLock()
 	defer pm.mutex.RUnlock()
 	Logger.Debugf("BroadcastRandom total peer size:%v", len(pm.peers))
@@ -300,7 +299,7 @@ func (pm *PeerManager) BroadcastRandom(packet *bytes.Buffer) {
 	if len(availablePeers) < maxCount {
 		for _, p := range availablePeers {
 			Logger.Debugf("BroadcastRandom send node id:%v", p.Id.GetHexString())
-			p.write(packet)
+			p.write(packet,code)
 		}
 	} else {
 		nodesHasSend := make(map[int]bool)
@@ -314,7 +313,7 @@ func (pm *PeerManager) BroadcastRandom(packet *bytes.Buffer) {
 			nodesHasSend[peerIndex] = true
 			p := availablePeers[peerIndex]
 			Logger.Debugf("BroadcastRandom send node id:%v", p.Id.GetHexString())
-			p.write(packet)
+			p.write(packet,code)
 		}
 	}
 
