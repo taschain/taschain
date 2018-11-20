e589838505a39bf18818e8095354778b91ff1817
FlyingSmallBear <1581487855@qq.com>
2018-11-20 10:04:03 +0800

    组同步和块同步日志分开打印

--- src/core/block_sync.go
+++ src/core/block_sync.go
@@ -31,6 +31,7 @@ const (
 )
 
 var BlockSyncer *blockSyncer
+var blockSyncLogger taslog.Logger
 
 type blockSyncer struct {
 	candidate blockSyncCandidate
@@ -59,9 +60,7 @@ type BlockInfo struct {
 }
 
 func InitBlockSyncer(isLightMiner bool) {
-	if logger == nil {
-		logger = taslog.GetLoggerByName("sync" + common.GlobalConf.GetString("instance", "index", ""))
-	}
+	blockSyncLogger = taslog.GetLoggerByName("block_sync" + common.GlobalConf.GetString("instance", "index", ""))
 	BlockSyncer = &blockSyncer{hasNeighbor: false, init: false, lightMiner: isLightMiner}
 	BlockSyncer.syncTimer = time.NewTimer(blockSyncInterval)
 	BlockSyncer.blockInfoNotifyTimer = time.NewTimer(blockSyncInterval)
@@ -90,13 +89,13 @@ func (bs *blockSyncer) loop() {
 
 func (bs *blockSyncer) SendTopBlockInfoToNeighbor(bi BlockInfo) {
 	bs.blockInfoNotifyTimer.Reset(blockSyncInterval)
-	logger.Debugf("[BlockSyncer]Send local total qn %d to neighbor!", bi.TotalQn)
+	blockSyncLogger.Debugf("[BlockSyncer]Send local total qn %d to neighbor!", bi.TotalQn)
 	if bi.Height == 0 {
 		return
 	}
 	body, e := marshalBlockInfo(bi)
 	if e != nil {
-		logger.Errorf("marshal blockInfo error:%s", e.Error())
+		blockSyncLogger.Errorf("marshal blockInfo error:%s", e.Error())
 		return
 	}
 	message := network.Message{Code: network.BlockInfoNotifyMsg, Body: body}
@@ -115,7 +114,7 @@ func (bs *blockSyncer) blockInfoHandler(msg notify.Message) {
 		return
 	}
 
-	logger.Debugf("[BlockSyncer] Rcv total qn from:%s,totalQN:%d,height:%d", bnm.Peer, blockInfo.TotalQn, blockInfo.Height)
+	blockSyncLogger.Debugf("[BlockSyncer] Rcv total qn from:%s,totalQN:%d,height:%d", bnm.Peer, blockInfo.TotalQn, blockInfo.Height)
 	if !bs.hasNeighbor {
 		bs.hasNeighbor = true
 	}
@@ -139,7 +138,7 @@ func (bs *blockSyncer) sync(candidate *blockSyncCandidate) {
 	if candidate.id == "" {
 		return
 	}
-	logger.Debugf("Start sync!")
+	blockSyncLogger.Debugf("Start sync!")
 	topBlock := BlockChainImpl.QueryTopBlock()
 	localTotalQN, localHash, localPreHash, localHeight := topBlock.TotalQN, topBlock.Hash, topBlock.PreHash, topBlock.Height
 	bs.lock.Lock()
@@ -147,9 +146,9 @@ func (bs *blockSyncer) sync(candidate *blockSyncCandidate) {
 	bs.lock.Unlock()
 
 	if candidateQN < localTotalQN || candidateHash == localHash {
-		logger.Debugf("[BlockSyncer]Neighbor chain's max totalQN: %d,is less than self chain's totalQN: %d.\nDon't sync!", candidateQN, localTotalQN)
+		blockSyncLogger.Debugf("[BlockSyncer]Neighbor chain's max totalQN: %d,is less than self chain's totalQN: %d.\nDon't sync!", candidateQN, localTotalQN)
 		if !bs.init {
-			logger.Info("Block first sync finished!")
+			blockSyncLogger.Info("Block first sync finished!")
 			bs.init = true
 		}
 
@@ -159,8 +158,8 @@ func (bs *blockSyncer) sync(candidate *blockSyncCandidate) {
 		return
 	}
 
-	logger.Debugf("[Sync]Neighbor Top hash:%v,height:%d,totalQn:%d,pre hash:%v,!", candidateHash.Hex(), candidateHeight, candidateQN, candidatePreHash.Hex())
-	logger.Debugf("[Sync]Local Top hash:%v,height:%d,totalQn:%d,pre hash:%v,!", localHash.Hex(), localHeight, localTotalQN, localPreHash.Hex())
+	blockSyncLogger.Debugf("[Sync]Neighbor Top hash:%v,height:%d,totalQn:%d,pre hash:%v,!", candidateHash.Hex(), candidateHeight, candidateQN, candidatePreHash.Hex())
+	blockSyncLogger.Debugf("[Sync]Local Top hash:%v,height:%d,totalQn:%d,pre hash:%v,!", localHash.Hex(), localHeight, localTotalQN, localPreHash.Hex())
 	if candidatePreHash == localHash || (candidatePreHash == localPreHash && candidateQN > localTotalQN) {
 		RequestBlock(candidateId, candidateHeight)
 		return
@@ -182,7 +181,7 @@ func unMarshalBlockInfo(b []byte) (*BlockInfo, error) {
 	message := new(tas_middleware_pb.BlockInfo)
 	e := proto.Unmarshal(b, message)
 	if e != nil {
-		logger.Errorf("unMarshalBlockInfo error:%s", e.Error())
+		blockSyncLogger.Errorf("unMarshalBlockInfo error:%s", e.Error())
 		return nil, e
 	}
 	blockInfo := BlockInfo{Hash: common.BytesToHash(message.Hash), TotalQn: *message.TotalQn, Height: *message.Height, PreHash: common.BytesToHash(message.PreHash)}
