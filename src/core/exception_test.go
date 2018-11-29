69672c8b91830669debf491963465db1dbebc620
liuhaox1 <feiling111222@gmail.com>
2018-11-29 11:37:20 +0800

    增加测试用例

--- src/core/exception_test.go
+++ src/core/exception_test.go
@@ -21,7 +21,7 @@ func hasData(address string,key string)bool{
 func TestCallError(t *testing.T) {
 	Clear()
 
-	code := tvm.Read0("../tvm/py/test/contract_exception.py")
+	code := tvm.Read0("../tvm/py/test/contract_call_exception.py")
 	contract := tvm.Contract{code, "ContractException", nil}
 	jsonString, _ := json.Marshal(contract)
 	OnChainFunc(string(jsonString), "0xff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b")
@@ -36,5 +36,18 @@ func TestCallError(t *testing.T) {
 }
 
 func TestSyntaxError(t *testing.T) {
+	Clear()
 
+	code := tvm.Read0("../tvm/py/test/contract_syntax_exception.py")
+	contract := tvm.Contract{code, "ContractSyntax", nil}
+	jsonString, _ := json.Marshal(contract)
+	OnChainFunc(string(jsonString), "0xff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b")
+
+	contractAddr := "0x2a4e0a5fb3d78a2c725a233b1bccff7560c35610"
+	abi := `{"FuncName": "callExcption1", "Args": []}`
+	CallContract(contractAddr, abi)
+
+	if !hasData(contractAddr,"data"){
+		t.Fatal("call contract failed.")
+	}
 }
