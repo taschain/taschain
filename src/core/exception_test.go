32f389c6732a061f8505530a5747acffc922d103
liuhaox1 <feiling111222@gmail.com>
2018-11-29 11:05:50 +0800

    修改测试用例

--- src/core/exception_test.go
+++ src/core/exception_test.go
@@ -10,19 +10,29 @@ func TestGasNotEnough(t *testing.T) {
 
 }
 
+func hasData(address string,key string)bool{
+	datas := GetContractDatas(address)
+	if datas[key] !=  `1"success"`{
+		return false
+	}
+	return true
+}
+
 func TestCallError(t *testing.T) {
 	Clear()
 
 	code := tvm.Read0("../tvm/py/test/contract_exception.py")
 	contract := tvm.Contract{code, "ContractException", nil}
 	jsonString, _ := json.Marshal(contract)
-	//fmt.Println(string(jsonString))
 	OnChainFunc(string(jsonString), "0xff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b")
 
 	contractAddr := "0x2a4e0a5fb3d78a2c725a233b1bccff7560c35610"
 	abi := `{"FuncName": "callExcption1", "Args": []}`
 	CallContract(contractAddr, abi)
 
+	if !hasData(contractAddr,"data"){
+		t.Fatal("call contract failed.")
+	}
 }
 
 func TestSyntaxError(t *testing.T) {
