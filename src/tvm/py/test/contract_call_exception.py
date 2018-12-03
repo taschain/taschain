c94e52dc506964e63eb5d0509951f9c26771b330
liuhaox1 <feiling111222@gmail.com>
2018-12-03 09:51:33 +0800

    修改测试用例

--- src/tvm/py/test/contract_call_exception.py
+++ src/tvm/py/test/contract_call_exception.py
@@ -14,11 +14,11 @@ class ContractException():
             xx = account.contractCall("0x2a4e0a5fb3d78a2c725a233b1bccff7560c35610", "be_calledException1", "[]")
         except Exception as e:
             print("====error is " + str(e))
-            if str(e).count("dsdsds")>0:
+            if str(e).count("ddd")>0:
                 self.data = "success"
 
     @register.public()
     def be_calledException1(self):
-        self.a=dsdsds
+        self.a=ddd
 
 
