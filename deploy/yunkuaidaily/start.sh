828abf39515806c58e9336e1bbfbe8ef45c72a70
FlyingSmallBear <1581487855@qq.com>
2018-12-13 10:16:49 +0800

    yunkuai daily start.sh 提交

--- deploy/yunkuaidaily/start.sh
+++ deploy/yunkuaidaily/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-instance_index=$1
-instance_count=$2
+instance_index=1
+instance_count=$1
 instance_end=$instance_index+$instance_count
-nat_server=$3
-build_number=$4
+
 
 for((;instance_index<instance_end;instance_index++))
 
@@ -19,18 +18,17 @@ do
 	rpc_port=$[8100+$instance_index]
 	pprof_port=$[9000+$instance_index]
 	config_file='tas'$instance_index'.ini'
-	stdout_log='logs/stdout_'$instance_index'.log'
+	stdout_log='logs/nohup_out_'$instance_index'.log'
 	pid_file='pid/pid_tas'$instance_index'.txt'
 
 	if [ -e $pid_file ];then
 		kill -9 `cat $pid_file`
 	fi
 
-	#echo -e 'nohup ./gtas miner --config' $config_file '--rpc --rpcport' $rpc_port '--super --instance' $instance_index '--pprof' $pprof_port '>' $stdout_log '2>&1 & echo $! >' $pid_file
-
 	if [ $instance_index -eq 1 ];then
-		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --prefix aly_flow --nat $nat_server --build_id $build_number --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port --super --instance $instance_index --pprof $pprof_port --test  --seed 120.77.41.14 > $stdout_log 2>&1 & echo $! > $pid_file
 	else
-		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --prefix aly_flow --nat $nat_server --build_id $build_number --pprof $pprof_port > $stdout_log 2>&1 & echo $! > $pid_file
+		nohup ./gtas miner --config $config_file --rpc --rpcport $rpc_port  --instance $instance_index --pprof $pprof_port --test  --seed 120.77.41.14 > $stdout_log 2>&1 & echo $! > $pid_file
 	fi
+	sleep 1
 done
