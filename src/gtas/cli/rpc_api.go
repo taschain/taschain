11132217b4f91c02a96560bcf2add82e68cf04a1
FlyingSmallBear <1581487855@qq.com>
2019-01-10 17:19:21 +0800

    1.交易入口返回合约地址 2.gas limit too large 返回显示最大值

--- src/gtas/cli/rpc_api.go
+++ src/gtas/cli/rpc_api.go
@@ -61,10 +61,13 @@ func (api *GtasAPI) Tx(txRawjson string) (*Result, error) {
 		return failResult(err.Error())
 	}
 
+	var contractAddr string
+	if trans.Type == types.TransactionTypeContractCreate {
+		contractAddr = common.BytesToAddress(common.Sha256(common.BytesCombine(trans.Source[:], common.Uint64ToByte(trans.Nonce)))).String()
+		return successResult(trans.Hash.String() + ",Contract Address:" + contractAddr)
+	}
 	return successResult(trans.Hash.String())
-
 }
-
 
 // Balance 查询余额接口
 func (api *GtasAPI) Balance(account string) (*Result, error) {
@@ -209,7 +212,6 @@ func (api *GtasAPI) GetBlockByHash(hash string) (*Result, error) {
 	}
 	return successResult(block)
 }
-
 
 func (api *GtasAPI) GetTopBlock() (*Result, error) {
 	bh := core.BlockChainImpl.QueryTopBlock()
@@ -572,7 +574,6 @@ func (api *GtasAPI) PageGetGroups(page, limit int) (*Result, error) {
 	return successResult(pageObject)
 }
 
-
 func (api *GtasAPI) BlockDetail(h string) (*Result, error) {
 	chain := core.BlockChainImpl
 	b := chain.QueryBlockByHash(common.HexToHash(h))
@@ -775,6 +776,6 @@ func (api *GtasAPI) Nonce(addr string) (*Result, error) {
 }
 
 func (api *GtasAPI) ContextSummary() (*Result, error) {
-    s := mediator.Proc.BlockContextSummary()
-    return successResult(s)
+	s := mediator.Proc.BlockContextSummary()
+	return successResult(s)
 }
