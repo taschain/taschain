12728b12517db3b6b539b73848baaa29bb6377b4
Yeatol <vim@vimer.org>
2019-01-11 11:48:59 +0800

    更新打包脚本

--- deploy/ops/windows/pkg_zip.bat
+++ deploy/ops/windows/pkg_zip.bat
@@ -1,8 +1,9 @@
 set version=0.5.1
-set GOPATH=%cd%\..\..\..\thirdparty;%cd%\..\..\..
+set GOPATH=%cd%\..\..\..\..\taschain_thirdparty;%cd%\..\..\..
+del gtas_windows_v%version%.zip
 cd gtas_windows
 go clean
-go build -o ./gtas.exe ../....//../src/gtas/main.go
+go build -o ./gtas.exe ../../../../src/gtas/main.go
 "C:\Program Files\7-Zip\7z.exe" a -tzip ..\gtas_windows_v%version%.zip *
 del gtas.exe
 cd ..
