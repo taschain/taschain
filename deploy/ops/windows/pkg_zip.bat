7c4cc1cdf8909fe368af4973fd73b28020f463bc
Yeatol <vim@vimer.org>
2019-01-11 11:41:45 +0800

    修改windows打包脚本路径

--- deploy/ops/windows/pkg_zip.bat
+++ deploy/ops/windows/pkg_zip.bat
@@ -1,8 +1,8 @@
 set version=0.5.1
-set GOPATH=%cd%\..\..\thirdparty;%cd%\..\..
+set GOPATH=%cd%\..\..\..\thirdparty;%cd%\..\..\..
 cd gtas_windows
 go clean
-go build -o ./gtas.exe ../../../src/gtas/main.go
+go build -o ./gtas.exe ../....//../src/gtas/main.go
 "C:\Program Files\7-Zip\7z.exe" a -tzip ..\gtas_windows_v%version%.zip *
 del gtas.exe
 cd ..
