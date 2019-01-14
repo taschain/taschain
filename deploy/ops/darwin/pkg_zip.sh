eeb577465436985731f49f63ac90aa01f65d98b0
FlyingSmallBear <1581487855@qq.com>
2019-01-14 15:54:08 +0800

    mac pkg.sh复原

--- deploy/ops/darwin/pkg_zip.sh
+++ deploy/ops/darwin/pkg_zip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-version=0.5.2
+version=0.5.1
 
-cd ../daily
+cd ../../daily
 sh build.sh
-cp -f gtas ../ops/gtas_mac
-cd ../ops
+cp -f gtas ../ops/darwin/gtas_mac
+cd ../ops/darwin
 zip -r gtas_mac_v${version}.zip gtas_mac
