37ccb90c8a991437ad41ecda08c979c070a4a985
FlyingSmallBear <1581487855@qq.com>
2019-01-14 16:00:52 +0800

    linux  pkg.sh复原

--- deploy/ops/linux/pkg_zip.sh
+++ deploy/ops/linux/pkg_zip.sh
@@ -2,8 +2,8 @@
 
 version=0.5.2
 
-cd ../daily
+cd ../../daily
 sh build.sh
-cp -f gtas ../ops/gtas_mac
-cd ../ops
-zip -r gtas_mac_v${version}.zip gtas_mac
+cp -f gtas ../ops/linux/gtas_linux
+cd ../ops/linux
+zip -r gtas_linux_v${version}.zip gtas_linux
