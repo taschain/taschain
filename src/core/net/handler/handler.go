66055c266dae426d4fb17a12b5533ddc012b1af0
FlyingSmallBear <1581487855@qq.com>
2019-01-14 17:27:37 +0800

    bug fix:新块广播也会触发分叉检查

--- src/core/net/handler/handler.go
+++ src/core/net/handler/handler.go
@@ -199,7 +199,7 @@ func (ch ChainHandler) newBlockHandler(msg notify.Message) {
 
 	core.Logger.Debugf("Rcv new block from %s,hash:%v,height:%d,totalQn:%d,tx len:%d", source, block.Header.Hash.Hex(), block.Header.Height, block.Header.TotalQN, len(block.Transactions))
 	//此处source传空，此处上链引发如果触发分叉不处理
-	core.BlockChainImpl.AddBlockOnChain("", block)
+	core.BlockChainImpl.AddBlockOnChain(source, block)
 }
 
 //----------------------------------------------------------------------------------------------------------------------
