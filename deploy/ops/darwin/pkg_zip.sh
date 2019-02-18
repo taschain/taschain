ca667245e1e5b09bab4a4642036d1e8f3200e8f6
jing <>
2019-02-18 14:38:59 +0800

    更新打包脚本，支持打包pylib

--- deploy/ops/darwin/pkg_zip.sh
+++ deploy/ops/darwin/pkg_zip.sh
@@ -6,4 +6,9 @@ cd ../../daily
 sh build.sh
 cp -f gtas ../ops/darwin/gtas_mac
 cd ../ops/darwin
+# copy tvm lib
+mkdir gtas_mac/py
+cp ../../../src/tvm/py/time.py ./gtas_mac/py
+cp ../../../src/tvm/py/coin.py ./gtas_mac/py
+#
 zip -r gtas_mac_v${version}.zip gtas_mac
