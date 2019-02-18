b6843b583f2290e004107a0d4586b0db878e4ac2
jing <>
2019-02-18 15:30:52 +0800

    tvm: 增加gc

--- src/gtas/cli/console.go
+++ src/gtas/cli/console.go
@@ -402,6 +402,9 @@ func (c *exportAbiCmd) parse(args []string) bool {
 
 func (c *exportAbiCmd) export () {
 	vm := tvm.NewTvm(nil, &c.contract, common.GlobalConf.GetString("tvm", "pylib", "py"))
+	defer func() {
+		vm.DelTvm()
+	}()
 	str := `
 class Register(object):
     def __init__(self):
--- src/tvm/bridge_wrap_darwin.go
+++ src/tvm/bridge_wrap_darwin.go
@@ -455,6 +455,7 @@ func (tvm *Tvm) Pycode2bytecode(str string) {
 func (tvm *Tvm) DelTvm() {
 	C.tvm_gas_report()
 	//TODO 释放tvm环境 tvmObj
+	C.tvm_gc()
 }
 
 func (tvm *Tvm) checkABI(abi ABI) (int,string) {
