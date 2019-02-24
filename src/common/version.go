865a3404c8be00601cf741695c9089a612334365
pxf <pxf@taschain.io>
2019-02-24 11:17:33 +0800

    version0.7.2

--- src/common/version.go
+++ src/common/version.go
@@ -9,6 +9,7 @@ package common
 //const GtasVersion = "0.6.2"
 //const GtasVersion = "0.6.3"	//添加日志推送到mysql
 //const GtasVersion = "0.7.0"	//建组优化，组成员可动态调整
-const GtasVersion = "0.7.1"	//修复同一个高度建多个组的bug,pong时参考高度
+//const GtasVersion = "0.7.1"	//修复同一个高度建多个组的bug,pong时参考高度
+const GtasVersion = "0.7.2"	//建组时某些节点sharePiece收不齐,新增请求sharePiece的功能
 
 const ConsensusVersion = 1
