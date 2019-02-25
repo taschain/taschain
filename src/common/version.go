88391e71b0c851e477b1f07c5e08e611960bfa17
问勤 <pxf@taschain.io>
2019-02-25 16:49:02 +0800

    0.7.4

--- src/common/version.go
+++ src/common/version.go
@@ -11,6 +11,7 @@ package common
 //const GtasVersion = "0.7.0"	//建组优化，组成员可动态调整
 //const GtasVersion = "0.7.1"	//修复同一个高度建多个组的bug,pong时参考高度
 //const GtasVersion = "0.7.2"	//建组时某些节点sharePiece收不齐齐,新增请求sharePiece的功能
-const GtasVersion = "0.7.3"	//p2p队列，交易池参数优化
+//const GtasVersion = "0.7.3"	//p2p队列，交易池参数优化
+const GtasVersion = "0.7.4"	//p2p参数改为1024
 
 const ConsensusVersion = 1
