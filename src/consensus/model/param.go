ae5092ebead25c7f0b750115fb5722913fe6015b
Feng Lei <fenglei108@163.com>
2019-05-22 14:14:07 +0800

    PotentialProposal =5 for testing

--- src/consensus/model/param.go
+++ src/consensus/model/param.go
@@ -85,7 +85,7 @@ func InitParam(cc common.SectionConfManager) {
 		GroupWorkGap:        uint64(cc.GetInt("group_cast_qualify_gap", GROUP_Work_GAP)),
 		GroupworkDuration:   uint64(cc.GetInt("group_cast_duration", GROUP_Work_DURATION)),
 		//EffectGapAfterApply: EPOCH,
-		PotentialProposal: 10,
+		PotentialProposal: 5,
 		ProposalBonus: 	common.TAS2RA(12),
 		PackBonus: common.TAS2RA(3),
 		VerifyBonus: common.TAS2RA(15),
