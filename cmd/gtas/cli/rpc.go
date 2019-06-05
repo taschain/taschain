675315ca7cbb82e44013e538564ccf4a31b778df
liuhao <feiling111222@gmail.com>
2019-06-05 18:38:55 +0800

    modify log  and package doc

--- cmd/gtas/cli/rpc.go
+++ cmd/gtas/cli/rpc.go
@@ -70,11 +70,11 @@ func StartRPC(host string, port uint) error {
 	for plus := 0; plus < 40; plus++ {
 		err = startHTTP(fmt.Sprintf("%s:%d", host, port+uint(plus)), apis, []string{}, []string{}, []string{})
 		if err == nil {
-			common.DefaultLogger.Infof("RPC serving on http://%s:%d\n", host, port+uint(plus))
+			common.DefaultLogger.Errorf("RPC serving on http://%s:%d\n", host, port+uint(plus))
 			return nil
 		}
 		if strings.Contains(err.Error(), "address already in use") {
-			common.DefaultLogger.Infof("address: %s:%d already in use\n", host, port+uint(plus))
+			common.DefaultLogger.Errorf("address: %s:%d already in use\n", host, port+uint(plus))
 			continue
 		}
 		return err
